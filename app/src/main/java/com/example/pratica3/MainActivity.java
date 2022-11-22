ccac0f5e1573268a1a29344c82d9bc286757eb41
Allysson Santos <68858220+Allyssonantos@users.noreply.github.com>
2022-11-21 21:08:48 -0300

    02

--- app/src/main/java/com/example/pratica3/MainActivity.java
+++ app/src/main/java/com/example/pratica3/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.pratica3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BreakIterator campoTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void mostraTexto (View view) {
+        // Recupera o texto digitado
+       
     }
 }
